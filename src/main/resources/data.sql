9aa0403c139fc16d5759e7e658a8bdb7f6edf988
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-04-12 13:12:59 +0800

    Add data.sql and schema.sql to initialize DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+insert into employee (name) values ('pas');
+insert into employee (name) values ('lucia');
+insert into employee (name) values ('lucas');
+insert into employee (name) values ('siena');
